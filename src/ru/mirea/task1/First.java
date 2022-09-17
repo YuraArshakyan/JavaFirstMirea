5d3ac0b214813093bcf719dcfc4da608a48b8298
Yura Arshakyan <yuro.arshakian@bk.ru>
2022-09-17 09:48:49 +0300

    ex. 1

--- src/ru/mirea/task1/First.java
+++ src/ru/mirea/task1/First.java
@@ -7,12 +7,26 @@ public class First {
         int s  = scan_of_num.nextInt();
         int[] array;
         array = new int[s];
-        int summ = 0;
+        int summ_FOR = 0;
+        int summ_While = 0;
+        int summ_DO_While = 0;
+        int i1 = 0;
+        int i2 = 0;
         for(int i = 0; i < s; i++){
             Scanner value = new Scanner(System.in);
             array[i] = value.nextInt();
-            summ += array[i];
+            summ_FOR += array[i];
         }
-        System.out.println( "The sum of array: " + summ); // Вывод сообщения на экран
+        System.out.println( "The sum of array(for): " + summ_FOR);
+        while (i1 < s){
+            summ_While += array[i1];
+            i1++;
+        }
+        System.out.println( "The sum of array(while): " + summ_While);
+        do{
+            summ_DO_While += array[i2];
+            i2++;
+        }while(i2 < s);
+        System.out.println( "The sum of array(while+do): " + summ_DO_While);
     }
 }
